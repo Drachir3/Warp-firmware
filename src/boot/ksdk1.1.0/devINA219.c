f65211715921448d79f1ce06a25fd4f581c5fef5
Richard Cavanagh <rac.cavanagh@gmail.com>
2021-12-08 17:38:06 +0000

    NOW up to date with cw 4

---
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -0,0 +1,208 @@
+#include <stdlib.h>
+
+#include "config.h"
+
+#include "fsl_misc_utilities.h"
+#include "fsl_device_registers.h"
+#include "fsl_i2c_master_driver.h"
+#include "fsl_spi_master_driver.h"
+#include "fsl_rtc_driver.h"
+#include "fsl_clock_manager.h"
+#include "fsl_power_manager.h"
+#include "fsl_mcglite_hal.h"
+#include "fsl_port_hal.h"
+
+#include "gpio_pins.h"
+#include "SEGGER_RTT.h"
+#include "warp.h"
+
+
+extern volatile WarpI2CDeviceState	deviceINA219State;
+extern volatile uint32_t		gWarpI2cBaudRateKbps;
+extern volatile uint32_t		gWarpI2cTimeoutMilliseconds;
+
+void
+initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts)
+{
+	deviceINA219State.i2cAddress			= i2cAddress;
+	deviceINA219State.operatingVoltageMillivolts	= operatingVoltageMillivolts;
+
+	return;
+}
+
+WarpStatus
+writeSensorRegisterINA219(uint8_t deviceRegister, uint8_t payloadMSB, uint8_t payloadLSB)
+{
+	uint8_t		payload[2], commandByte[1];
+	i2c_status_t	status;
+
+	switch (deviceRegister)
+	{
+		case 0x00: case 0x05: 
+		{
+			/* OK */
+			break;
+		}
+		
+		default:
+		{
+			return kWarpStatusBadDeviceCommand;
+		}
+	}
+
+	i2c_device_t slave =
+	{
+		.address = deviceINA219State.i2cAddress,
+		.baudRate_kbps = gWarpI2cBaudRateKbps
+	};
+
+	warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
+	commandByte[0] = deviceRegister;
+	payload[0] = payloadMSB;
+	payload[1] = payloadLSB;
+	warpEnableI2Cpins();
+	
+	status = I2C_DRV_MasterSendDataBlocking(
+							0 /* I2C instance */,
+							&slave,
+							commandByte,
+							1,
+							payload,
+							2,			/* Payload size (no. of bytes) */
+							gWarpI2cTimeoutMilliseconds);
+	if (status != kStatus_I2C_Success)
+	{
+		return kWarpStatusDeviceCommunicationFailed;
+	}
+
+	return kWarpStatusOK;
+}
+
+WarpStatus
+configureSensorINA219(uint8_t payloadConfigurationRegisterMSB, uint8_t payloadConfigurationRegisterLSB, uint8_t payloadCalibrationMSB,  uint8_t payloadCalibrationLSB)
+{
+	WarpStatus	i2cWriteStatus1, i2cWriteStatus2;
+
+	warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
+
+	// VBUS_MAX = 32V             (Assumes 32V, can also be set to 16V)
+	// VSHUNT_MAX = 0.32          (Assumes Gain 8, 320mV, can also be 0.16, 0.08,
+	// 0.04) RSHUNT = 0.1               (Resistor value in ohms)
+
+	// 1. Determine max possible current
+	// MaxPossible_I = VSHUNT_MAX / RSHUNT
+	// MaxPossible_I = 3.2A
+
+	// 2. Determine max expected current
+	// MaxExpected_I = 2.0A
+
+	// 3. Calculate possible range of LSBs (Min = 15-bit, Max = 12-bit)
+	// MinimumLSB = MaxExpected_I/32767
+	// MinimumLSB = 0.000061              (61uA per bit)
+	// MaximumLSB = MaxExpected_I/4096
+	// MaximumLSB = 0,000488              (488uA per bit)
+
+	// 4. Choose an LSB between the min and max values
+	//    (Preferrably a roundish number close to MinLSB)
+	// CurrentLSB = 0.0001 (100uA per bit)
+
+	// 5. Compute the calibration register
+	// Cal = trunc (0.04096 / (Current_LSB * RSHUNT))
+	
+	// Cal = 4096 (0x1000)
+
+	
+  
+	i2cWriteStatus1 = writeSensorRegisterINA219(kWarpSensorConfigurationRegisterINA219ConfigurationRegister /* Register address for Configuration Register */,
+							payloadConfigurationRegisterMSB,
+							payloadConfigurationRegisterLSB /* Input to register */
+							);
+	
+	i2cWriteStatus2 = writeSensorRegisterINA219(kWarpSensorConfigurationRegisterINA219Cablibration /* Register address for Calibration register */,
+							payloadCalibrationMSB,
+							payloadCalibrationLSB  /* Input to register */
+							);
+
+	return (i2cWriteStatus1 | i2cWriteStatus2);
+}
+
+WarpStatus
+readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes)
+{
+	uint8_t		cmdBuf[1] = {0xFF};
+	i2c_status_t	status;
+	
+	USED(numberOfBytes);
+	switch (deviceRegister)
+	{
+		case 0x00: case 0x01: case 0x02: case 0x03: 
+		case 0x04: case 0x05: 
+		{
+			/* OK */
+			break;
+		}
+		
+		default:
+		{
+			return kWarpStatusBadDeviceCommand;
+		}
+	}
+
+	i2c_device_t slave =
+	{
+		.address = deviceINA219State.i2cAddress,
+		.baudRate_kbps = gWarpI2cBaudRateKbps
+	};
+	
+	warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
+	cmdBuf[0] = deviceRegister;
+	warpEnableI2Cpins();
+	
+	status = I2C_DRV_MasterReceiveDataBlocking(
+							0 /* I2C peripheral instance */,
+							&slave,
+							cmdBuf,
+							1,
+							(uint8_t *)deviceINA219State.i2cBuffer,
+							numberOfBytes,
+							gWarpI2cTimeoutMilliseconds);
+
+	if (status != kStatus_I2C_Success)
+	{
+		return kWarpStatusDeviceCommunicationFailed;
+	}
+
+	return kWarpStatusOK;
+}
+
+void
+printSensorDataINA219(bool hexModeFlag)
+{
+	uint16_t	readSensorRegisterValueLSB;
+	uint16_t	readSensorRegisterValueMSB;
+	int16_t		readSensorRegisterValueCombined;
+	WarpStatus	i2cReadStatus;
+
+	warpScaleSupplyVoltage(deviceINA219State.operatingVoltageMillivolts);
+
+	i2cReadStatus = readSensorRegisterINA219(kWarpSensorOutputRegisterINA219Current, 2 /* numberOfBytes */);
+	readSensorRegisterValueMSB = deviceINA219State.i2cBuffer[0];
+	readSensorRegisterValueLSB = deviceINA219State.i2cBuffer[1];
+	readSensorRegisterValueCombined = ((readSensorRegisterValueMSB & 0xFF) << 6) | (readSensorRegisterValueLSB >> 2);
+
+	if (i2cReadStatus != kWarpStatusOK)
+	{
+		warpPrint(" ----,");
+	}
+	else
+	{
+		if (hexModeFlag)
+		{
+			warpPrint(" 0x%02x 0x%02x,", readSensorRegisterValueMSB, readSensorRegisterValueLSB);
+		}
+		else
+		{
+			warpPrint(" %d,", readSensorRegisterValueCombined);
+		}
+	}
+}
